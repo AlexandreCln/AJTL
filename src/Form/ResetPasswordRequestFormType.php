f06d9fcffe0aac6e9bbc4ee7837a9512921967af
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-05-06 11:33:46 +0200

    Add fields to ResetPasswordRequestFormType

--- src/Form/ResetPasswordRequestFormType.php
+++ src/Form/ResetPasswordRequestFormType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ResetPasswordRequestFormType extends AbstractType
@@ -14,9 +15,17 @@ class ResetPasswordRequestFormType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class, [
+                'required' => true,
+                'attr' => [
+                    'label' => 'Email',
+                    'placeholder' => 'Entrez votre email'
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter your email',
+                    ]),
+                    new Email([
+                        'message' => 'Please enter a valid email address',
                     ]),
                 ],
             ])
